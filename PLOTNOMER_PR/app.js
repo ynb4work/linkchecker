92378b6aff40f1a3766f6ff0c6be077940abef93
ynb4m <ynb4work@gmail.com>
2025-04-08 16:08:17 +0300

    main

--- PLOTNOMER_PR/app.js
+++ PLOTNOMER_PR/app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const config = require("config");
 const cors = require("cors");
-const https = require("https");
-const fs = require("fs");
+const http = require("http");
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(express.json({ extended: true }));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
 app.use(express.raw({ type: '*/*' }));
+
 app.use(cors());
 
 app.all("*", (req, res) => {
@@ -18,56 +20,19 @@ app.all("*", (req, res) => {
   console.log("URL:", req.originalUrl);
   console.log("Заголовки:", JSON.stringify(req.headers, null, 2));
   console.log("Тело запроса:", req.body);
+
   res.status(200).send("OK");
 });
 
 const PORT = config.get("port") || 5000;
 
-// Замените пути к файлам сертификата
-const options = {
-  key: fs.readFileSync('privkey.pem'),
-  cert: fs.readFileSync('fullchain.pem')
-};
+async function start() {
+  try {
+    server.listen(PORT, () => console.log("Сервер запущен на порту", PORT));
+  } catch (e) {
+    console.log("Ошибка сервера:", e.message);
+    process.exit(1);
+  }
+}
 
-https.createServer(options, app).listen(PORT, () => {
-  console.log(`HTTPS сервер запущен на порту ${PORT}`);
-});
-
-// const express = require("express");
-// const config = require("config");
-// const cors = require("cors");
-// const http = require("http");
-
-// const app = express();
-// const server = http.createServer(app);
-
-// app.use(express.json({ extended: true }));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.text());
-// app.use(express.raw({ type: '*/*' }));
-
-// app.use(cors());
-
-// app.all("*", (req, res) => {
-//   console.log("Новый запрос:");
-//   console.log("Метод:", req.method);
-//   console.log("URL:", req.originalUrl);
-//   console.log("Заголовки:", JSON.stringify(req.headers, null, 2));
-//   console.log("Тело запроса:", req.body);
-
-//   res.status(200).send("OK");
-// });
-
-// const PORT = config.get("port") || 5000;
-
-// async function start() {
-//   try {
-//     server.listen(PORT, () => console.log("Сервер запущен на порту", PORT));
-//   } catch (e) {
-//     console.log("Ошибка сервера:", e.message);
-//     process.exit(1);
-//   }
-// }
-
-// start();
+start();
